329584b9ae256b03072d7a3dcfb6a8316cbe78ec
ProjectXundefined <projectxundefined@protonmail.com>
2022-12-14 15:08:39 +0100

    minor fixes
    minor fixes

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,17 @@
 
 const URL = 'https://swapi.dev/api/';
 
-getFilms();
-getPeople();
-getPlanet();
-getSpecies();
+onload = [
+    getFilms(),
+    generateFilmsLoading(),
+    getPeople(),
+    generatePeopleLoading(),
+    getPlanet(),
+    generatePlanetLoading(),
+    getSpecies(),
+    generateSpeciesLoading()
+];
+
 
 // Eventlistener buttons
 
@@ -16,22 +23,34 @@ document.getElementById("getspecies").addEventListener("click", getSpecies);
 
 // api function to fetch random film info 
 
-async function getFilms(){
+async function getFilms(){ 
     generateFilmsLoading();
 
     const getFilmsObject = await fetch(URL + 'films');
     const filmsData = await getFilmsObject.json();
-    const filmsObjectlength = Object.values(filmsData);
-    length = filmsObjectlength[0];
+    const filmsObjectCount = Object.values(filmsData);
+    count = filmsObjectCount[0];
 
-    let randomFilm = Math.floor((Math.random() * length) + 1);
+    let randomFilm = Math.floor((Math.random() * count) + 1);
     const response = await fetch(URL + 'films/' + randomFilm);
+    let lastRandomFilm = parseInt(response.url.substring(response.url.lastIndexOf('/') + 1));
+    // sessionStorage.setItem(lastRandomFilm)
+
     const data = await response.json();
     const { title, director, release_date } = data;
 
-    document.getElementById('film_title').textContent = title;
-    document.getElementById('film_director').textContent = director;
-    document.getElementById('film_release_date').textContent = release_date;
+    if (response.status === 404) {
+        document.getElementById('film_title').textContent = `Sorry ... could not load data. Error ${response.status}`;
+        document.getElementById('film_director').textContent = `Sorry ... could not load data. Error ${response.status}`;
+        document.getElementById('film_release_date').textContent = `Sorry ... could not load data. Error ${response.status}`;
+    } else {
+        document.getElementById('film_title').textContent = title;
+        document.getElementById('film_director').textContent = director;
+        document.getElementById('film_release_date').textContent = release_date;
+    };
+    console.log(response.url);
+    console.log(randomFilm);
+    console.log(lastRandomFilm);
 
 };
 
@@ -50,18 +69,23 @@ async function getPeople(){
 
     const getPeopleObject = await fetch(URL + 'people');
     const peopleData = await getPeopleObject.json();
-    const peopleObjectlength = Object.values(peopleData);
-    length = peopleObjectlength[0];
+    const peopleObjectCount = Object.values(peopleData);
+    count = peopleObjectCount[0];
 
-    let randomPerson = Math.floor((Math.random() * length) + 1);
+    let randomPerson = Math.floor((Math.random() * count) + 1);
     const response = await fetch(URL + 'people/' + randomPerson);
     const data = await response.json();
     const { name, height, birth_year } = data;
 
-    document.getElementById('person_name').textContent = name;
-    document.getElementById('person_birth_year').textContent = birth_year;
-    document.getElementById('person_height').textContent = height;
-
+    if (response.status === 404) {
+        document.getElementById('person_name').textContent = `Sorry ... could not load data. Error ${response.status}`;
+        document.getElementById('person_birth_year').textContent = `Sorry ... could not load data. Error ${response.status}`;
+        document.getElementById('person_height').textContent = `Sorry ... could not load data. Error ${response.status}`;
+    } else {
+        document.getElementById('person_name').textContent = name;
+        document.getElementById('person_birth_year').textContent = birth_year;
+        document.getElementById('person_height').textContent = height;
+    };
 };
 
 // function to visualize people loading  
@@ -79,18 +103,28 @@ async function getPlanet(){
 
     const getPlanetsObject = await fetch(URL + 'planets');
     const planetsData = await getPlanetsObject.json();
-    const planetssObjectlength = Object.values(planetsData);
-    length = planetssObjectlength[0];
+    const planetssObjectCount = Object.values(planetsData);
+    count = planetssObjectCount[0];
 
-    let randomPlanet = Math.floor((Math.random() * 60) + 1);
+    let randomPlanet = Math.floor((Math.random() * count) + 1);
     const response = await fetch(URL + 'planets/' + randomPlanet);
     const data = await response.json();
     const { name, population, climate } = data;
 
-    document.getElementById('planet_name').textContent = name;
-    document.getElementById('planet_population').textContent = population;
-    document.getElementById('planet_climate').textContent = climate;
-
+    if (response.status === 404) {
+        document.getElementById('planet_name').textContent = `Sorry ... could not load data. Error ${response.status}`;
+        document.getElementById('planet_population').textContent = `Sorry ... could not load data. Error ${response.status}`;
+        document.getElementById('planet_climate').textContent = `Sorry ... could not load data. Error ${response.status}`;
+    } else {
+        document.getElementById('planet_name').textContent = name;
+        if (population === "unknown" || NaN){ 
+            document.getElementById('planet_population').textContent = population
+        } else {
+            const internationalNumberFormat = new Intl.NumberFormat('en-US');
+            document.getElementById('planet_population').textContent = internationalNumberFormat.format(population);
+        }
+        document.getElementById('planet_climate').textContent = climate;
+    };
 };
 
 // function to visualize planets loading  
@@ -101,25 +135,30 @@ function generatePlanetLoading() {
     planet_climate.innerHTML        = '<i class="fas fa-circle-notch fa-spin fa-sw"></i>'
 };
 
-// api funtion to fetch random planet info 
+// api function to fetch random species info 
 
 async function getSpecies(){
     generateSpeciesLoading();
 
     const getSpeciesObject = await fetch(URL + 'species');
     const speciesData = await getSpeciesObject.json();
-    const speciesObjectlength = Object.values(speciesData);
-    length = speciesObjectlength[0];
+    const speciesObjectCount = Object.values(speciesData);
+    count = speciesObjectCount[0];
 
-    let randomSpecies = Math.floor((Math.random() * 37) + 1);
+    let randomSpecies = Math.floor((Math.random() * count) + 1);
     const response = await fetch(URL + 'species/' + randomSpecies);
     const data = await response.json();
     const { name, classification, language } = data;
-
-    document.getElementById('species_name').textContent = name;
-    document.getElementById('species_classification').textContent = classification;
-    document.getElementById('species_language').textContent = language;
-
+    
+    if (response.status === 404) {
+        document.getElementById('species_name').textContent = `Sorry ... could not load data. Error ${response.status}`;
+        document.getElementById('species_classification').textContent = `Sorry ... could not load data. Error ${response.status}`;
+        document.getElementById('species_language').textContent = `Sorry ... could not load data. Error ${response.status}`;
+    } else {
+        document.getElementById('species_name').textContent = name;
+        document.getElementById('species_classification').textContent = classification;
+        document.getElementById('species_language').textContent = language;
+    };
 };
 
 // function to visualize species loading  
